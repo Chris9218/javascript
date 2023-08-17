e8951f845fa235b721f6dfedc5ad837da7f05ab0
Chris9218 <christopher.tomini@gmail.com>
2023-08-17 12:56:51 +0200

    .

--- dom/EsercizioForm/index.js
+++ dom/EsercizioForm/index.js
@@ -1,6 +1,6 @@
 import {persons} from "/persons.js";
 import {populatePersonsList, populateOptions} from "/populatepersonlist.js";
-//import {personCreateForm, birthdayForm} from "./personcreate";
+import {Person} from "./types";
 
 
 
@@ -8,4 +8,34 @@ import {populatePersonsList, populateOptions} from "/populatepersonlist.js";
 document.addEventListener("listUpdated", () => {
     populatePersonsList(persons.list);
     populateOptions(persons.list);
+})
+
+const personCreateForm = document.forms.item(0);
+personCreateForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    e.stopPropagation();
+    const form = e.target;
+    const name = form.elements[0].value;
+    const city = form.elements[2].value;
+    const age = form.elements[1].value;
+    const person = new Person(name, age, city);
+
+    persons.addItemToList(person);
+
+    form.reset();
+
+})
+
+const birthdayForm = document.forms[1];
+birthdayForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    e.stopPropagation();
+    const selectedPersonName = e.target.elements[0].value;
+    const selectedPerson = persons.list.find(p => (p.name + "-" + p.age + "-" + p.city) === selectedPersonName);
+    if (selectedPerson instanceof Person){
+        selectedPerson.age++;
+        document.dispatchEvent(new Event('listUpdated', {
+            bubbles:true
+        }));
+    }
 })
